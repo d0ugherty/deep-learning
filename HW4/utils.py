2faabef21800266fbf298ae1e78a66f6cb6c45a4
d0ugherty <tdougherty16@gmail.com>
2023-10-14 16:38:03 -0400

    added loader creators

--- HW4/utils.py
+++ HW4/utils.py
@@ -1,6 +1,10 @@
 import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import DatasetFolder
 
-class DatasetStatistics:
+
+class DatasetUtils:
     def __init__(self, data_loader):
         """
         Initialize the DatasetStatistics class with a DataLoader object.
@@ -10,6 +14,23 @@ class DatasetStatistics:
         """
         self.data_loader = data_loader
         self.N = len(self.data_loader.dataset)
+
+    """ Just a helper functions for internal use"""
+    def _create_transforms(is_test, mean, std):
+        if(is_test):
+            transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=mean, std=std)
+            ])
+        else:
+            transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.Normalize(mean=mean, std=std)
+            ])
+        return transform
+
 
     def calculate_mean(self):
         """
@@ -43,3 +64,58 @@ class DatasetStatistics:
             std += ((images - mean.unsqueeze(1)) ** 2).sum([0, 2])
         std = torch.sqrt(std / (self.N * images.size(2)))
         return std
+    
+
+    @staticmethod
+    def create_loader_and_transform(root_path, loader_func, extensions,
+                                     batch_size=32,shuffle=True,is_test=True):
+        
+        # Step 1: Create initial DataLoader with ToTensor() transform for the entire dataset
+        initial_dataset = DatasetFolder(root=root_path, loader=loader_func,
+                                        extensions=extensions, transform=transforms.ToTensor())
+        
+        initial_loader = DataLoader(initial_dataset, batch_size=batch_size, shuffle=shuffle)
+    
+        if(is_test):
+            return DatasetUtils.create_test_loader(initial_dataset, initial_loader)
+        else:    
+            # Step 2: Randomly split the dataset into training and validation sets
+            train_set, val_set = random_split(initial_dataset, [1200, 600])
+            
+            # Step 3: Create DataLoaders for training and validation sets
+            
+            train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=shuffle)
+            val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=shuffle)  # Usually, we don't shuffle the validation set
+
+            # Step 4: Calculate statistics for the training set
+            stats_calc = DatasetUtils(train_loader)
+            mean = stats_calc.calculate_mean()
+            std = stats_calc.calculate_std(mean)
+
+            # Step 5: Create DataLoaders with full transform pipeline for both training and validation sets
+            final_transform = DatasetUtils._create_transforms(is_test, mean, std)
+            train_set.dataset.transform = final_transform
+            val_set.dataset.transform = final_transform
+
+            train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=shuffle)
+            val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
+            
+            return train_loader, val_loader, mean, std
+        
+    
+    @staticmethod
+    def create_test_loader_and_transform(init_dset, initial_loader):
+
+        stats_calc = DatasetUtils(initial_loader)
+        mean = stats_calc.calculate_mean()
+        std = stats_calc.calculate_std(mean)
+
+        final_transform = DatasetUtils.create_transforms(True, mean, std)
+        
+        final_dataset = DatasetFolder(root=init_dset.root_path, loader=init_dset.loader_func,
+                                        extensions=init_dset.extensions, transform=final_transform)
+        
+        final_loader = DataLoader(final_dataset, batch_size=init_dset.batch_size, shuffle=False)
+
+        return final_loader, mean, std
+   
