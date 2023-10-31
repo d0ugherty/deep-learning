8a0b80ae21f0296c2db459b7904c7106f8641e1c
thomas <tdougherty16@gmail.com>
2023-10-31 14:22:40 -0400

    read files and iterate over them

--- midterm_project/data_convert.py
+++ midterm_project/data_convert.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from scipy.io import loadmat
 import pandas as pd
 
@@ -6,8 +7,17 @@ import pandas as pd
 """
     Read file into python
 """
+parser = argparse.ArgumentParser(description='Convert MATLAB .gdf files in a directory to a Python-readable format.')
+parser.add_argument('dir_path', type=str, help='The path of the directory containing .gdf or .mat files to convert.')
 
+args = parser.parse_args()
+dir_path = args.dir_path
 
+file_list = os.listdir(dir_path)
+
+for file in file_list:
+  #  print(file)
+    pass
 """
     Convert file data 
 """
