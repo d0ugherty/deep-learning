9fb57867b42ce969e6a12dd9e9c89185d4abb3d4
thomas <tdougherty16@gmail.com>
2023-10-31 15:00:02 -0400

    imported MNE library to extract raw .gdf data

--- midterm_project/data_convert.py
+++ midterm_project/data_convert.py
@@ -1,11 +1,14 @@
 import argparse
 import os
 from scipy.io import loadmat
+import mne
 import pandas as pd
 
 
 """
     Read file into python
+    Takes a directory as a command line argument and iterates over 
+    each .gdf file to convert it into a python-readable format
 """
 parser = argparse.ArgumentParser(description='Convert MATLAB .gdf files in a directory to a Python-readable format.')
 parser.add_argument('dir_path', type=str, help='The path of the directory containing .gdf or .mat files to convert.')
@@ -16,8 +19,10 @@ dir_path = args.dir_path
 file_list = os.listdir(dir_path)
 
 for file in file_list:
-  #  print(file)
-    pass
+    if file.endswith('.gdf'):
+        raw_data = mne.io.read_raw_gdf
+        print(file)
+        print(type(raw_data))
 """
     Convert file data 
 """
