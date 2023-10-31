cb5d804d42a543b9402aa3032cfee9ec5e0b7be4
thomas <tdougherty16@gmail.com>
2023-10-31 13:50:44 -0400

    starting data conversion script

---
+++ midterm_project/data_convert.py
@@ -0,0 +1,18 @@
+import argparse
+from scipy.io import loadmat
+import pandas as pd
+
+
+"""
+    Read file into python
+"""
+
+
+"""
+    Convert file data 
+"""
+
+
+"""
+    Save converted file data
+"""
